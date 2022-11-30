3f5884cae3146fb62c2433f28db3b0f7f89bdaf7
tx.fan <vintinshaw@gmail.com>
2022-11-30 15:44:26 +0800

    Create Linux-v6login.sh

--- Linux-v6login.sh
+++ Linux-v6login.sh
@@ -1,5 +1,5 @@
 v6front="http://192.168.100.200/1.htm?mv6="
-v6back=$(ip -6 address show | grep inet6 |grep "global temporary dynamic" | awk '{print $2}' | cut -d'/' -f1)
+v6back=$(ip -6 address show | grep inet6 |grep "mngtmpaddr" | awk '{print $2}' | cut -d'/' -f1)
 v6all=$v6front$v6back
-#echo $v6all
+echo $v6all
 curl $v6all >> v6login.log 2>&1
